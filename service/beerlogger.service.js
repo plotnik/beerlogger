a6ba07711ff72e2c845418072cc52b1294f86076
Egor Abramovich <egor.abramovich@gmail.com>
2022-04-01 22:19:39 +0300

    checkTopics

--- service/beerlogger.service.js
+++ service/beerlogger.service.js
@@ -1,14 +1,20 @@
 const mysql = require('./mysql.service');
 const mqtt = require('./mqtt.service');
+const config = require('./config.service');
+
+var timer;
 
 function startLogger() {
     console.log('--- start');
-    mysql.start().then(mqtt.start());
+    mysql.start().then(mqtt.start().then(() => {
+        timer = setInterval(mqtt.checkTopics, config.refresh_topics_sec*1000);
+    }));
     return { success: true };
 }
 
 function stopLogger() {
     console.log('--- stop');
+    clearInterval(timer);
     mqtt.stop();
     return { success: true };
 }
--- service/mqtt.service.js
+++ service/mqtt.service.js
@@ -5,90 +5,60 @@ const config = require('./config.service');
 let client;
 
 let topics = [];
-let updatedTopics = [];
-let lastTopicsUpdate = Date.now();
+
 let tempData = {};
 let allData = {};
 let isWorking = false;
 
-function MQTTconnect() {
-    mysql._getTopics().then(data => {
-        topics = data;
+async function MQTTconnect() {
+    return new Promise((resolve, reject) => {
+        mysql._getTopics().then(data => {
+            topics = data;
 
-        console.log('--- MQTTconnect:', topics);
+            console.log('--- MQTTconnect:', topics);
 
-        var endpoint = 'wss://' + config.wqtt_host + ':' + config.wqtt_wss_port;
-        let options = {
-            username: config.wqtt_username,
-            password: config.wqtt_password,
-        };
+            var endpoint = 'wss://' + config.wqtt_host + ':' + config.wqtt_wss_port;
+            let options = {
+                username: config.wqtt_username,
+                password: config.wqtt_password,
+            };
 
-        isWorking = true;
-        client = mqttjs.connect(endpoint, options);
-        client.on('connect', () => {
-            for (let i = 0; i < topics.length; i++) {
-                client.subscribe(topics[i].topic);
-                //console.log('--- subscribe:', topics[i].topic);
-            }
-        });
-        client.on('message', function (topic, message) {
-
-            /* Обработать сообщения.
-             */
-            //console.log('--- message:', topic);
-            for (let i = 0; i < topics.length; i++) {
-                if (topic === topics[i].topic) {
-                    tempData = {
-                        "tstamp": timestamp(),
-                        "sensor": parseFloat(message.toString()),
-                        "node": topics[i].node,
-                        "site": topics[i].site
-                    }
-                    let itx = topics[i].site + '/' + topics[i].node;
-                    allData[itx] = tempData;
-                    mysql.writeSensorsData(tempData);
-                    //console.log('--------------- data:',tempData);
-                    //               writeSensorsData
+            isWorking = true;
+            client = mqttjs.connect(endpoint, options);
+            client.on('connect', () => {
+                for (let i = 0; i < topics.length; i++) {
+                    client.subscribe(topics[i].topic);
+                    //console.log('--- subscribe:', topics[i].topic);
                 }
-            }
+            });
+            client.on('message', function (topic, message) {
 
-            /* Проверить изменения в топиках.
-             */
-            const curTime = Date.now();
-            if (curTime - lastTopicsUpdate > config.refresh_topics_sec*1000) {
-                lastTopicsUpdate = curTime;
-                mysql._getTopics().then(data => {
-
-                    let newTopics = topicsDiff(data, topics);
-                    if (newTopics.length > 0) {
-                        console.log('--- newTopics:', newTopics);
-                        for (let i = 0; i < newTopics.length; i++) {
-                            client.subscribe(newTopics[i].topic);
+                /* Обработать сообщения.
+                */
+                //console.log('--- message:', topic);
+                for (let i = 0; i < topics.length; i++) {
+                    if (topic === topics[i].topic) {
+                        tempData = {
+                            "tstamp": timestamp(),
+                            "sensor": parseFloat(message.toString()),
+                            "node": topics[i].node,
+                            "site": topics[i].site
                         }
-                        topics = data;
+                        let itx = topics[i].site + '/' + topics[i].node;
+                        allData[itx] = tempData;
+                        mysql.writeSensorsData(tempData);
+                        //console.log('--------------- data:',tempData);
+                        //               writeSensorsData
                     }
+                }
 
-                    let oldTopics = topicsDiff(topics, data);
-                    if (oldTopics.length > 0) {
-                        console.log('--- oldTopics:', oldTopics);
-                        for (let i = 0; i < oldTopics.length; i++) {
-                            client.unsubscribe(oldTopics[i].topic);
-                        }
-                        topics = data;
-                    }
-                });
-            }
+            });
+
+            resolve();
+        }, () => {
+            reject();
         });
     });
-}
-
-/**
- * Найти разницу `topics1 - topics2`.
- */
-function topicsDiff(topics1, topics2) {
-    return topics1.filter(t1 => !topics2.some(t2 => {
-        return t1.topic === t2.topic;
-    }));
 }
 
 function timestamp() {
@@ -112,8 +82,45 @@ function getCurrentTopics() {
     return topics;
 }
 
+
+/* Проверить изменения в топиках.
+    */
+function checkTopics() {
+    //console.log('--- checkTopics');
+    mysql._getTopics().then(data => {
+
+        let newTopics = topicsDiff(data, topics);
+        if (newTopics.length > 0) {
+            console.log('--- newTopics:', newTopics);
+            for (let i = 0; i < newTopics.length; i++) {
+                client.subscribe(newTopics[i].topic);
+            }
+            topics = data;
+        }
+
+        let oldTopics = topicsDiff(topics, data);
+        if (oldTopics.length > 0) {
+            console.log('--- oldTopics:', oldTopics);
+            for (let i = 0; i < oldTopics.length; i++) {
+                client.unsubscribe(oldTopics[i].topic);
+            }
+            topics = data;
+        }
+    });
+}
+
+/**
+ * Найти разницу `topics1 - topics2`.
+ */
+ function topicsDiff(topics1, topics2) {
+    return topics1.filter(t1 => !topics2.some(t2 => {
+        return t1.topic === t2.topic;
+    }));
+}
+
 module.exports.start = MQTTconnect;
 module.exports.stop = stopProcessing;
 module.exports.readSensorsData = readSensorsData;
 module.exports.getCurrentTopics = getCurrentTopics;
-module.exports.topicsDiff = topicsDiff;
+module.exports.checkTopics = checkTopics;
+
